5a588a3e418e5e82307b182d551e8320dbd8d493
Ayumi <aokamoto@europa-group.com>
2023-10-11 12:23:14 +0200

    styles.css

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require('path');
 
 const app = express();
 
+// static code
+app.use(express.static(path.join(__dirname, "public")));
+
 // middleware CORS
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
